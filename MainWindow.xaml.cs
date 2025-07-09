b5e2df8afbbe61279e9f09a4b2d77e9295e0342b
eshumphries <eshumphries@outlook.com>
2025-07-09 15:54:28 -0400

    Added rooms and started on the first minion

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,14 +9,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MathMage;
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace MathMage 
 {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
     }
 }
