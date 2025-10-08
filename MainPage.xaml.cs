3d5abca50bddf6d418b7d986d1636a2acf29626e
eshumphries <eshumphries@outlook.com>
2025-10-08 16:41:26 -0400

    Went back to the previous idea of having one Randomizer method to randomize the math signs and numbers to display above the room doors for player choice

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -274,7 +274,7 @@ Noooooooooooooooooo!""";
 |  | __   __   __ |  | 
 |  ||. | |. | |. ||  | 
 |  ||__|_|__|_|__||  | 
-| / _?_?_?_?_?_=__ \ | 
+| /   0 ? _ =      \ | 
 |/__________________\|";
             asdLevel = 111;
             gameLevel = 1.1;
@@ -302,197 +302,26 @@ Noooooooooooooooooo!""";
 
             return asciiStringBuilder.ToString();
         }
-        public StringBuilder RoomNumbers(StringBuilder stringBuild)
+        public StringBuilder Randomizer(StringBuilder stringBuild)
         {
-            List<string> randNums = new List<string>();
             Random randNum = new Random();
-
-            int levelInt = ((int)Math.Abs(gameLevel) - 1) * 6 + 12;
-
-            //switch (levelInt)
-            //{
-            //    case 3:
-            //        stringBuild.Replace("   ", "3-6", 36, 3);
-            //        break;
-            //    case 24:
-            //        stringBuild.Replace("     ", "24-48", 36, 5);
-            //        break;
-            //    case 81:
-            //        stringBuild.Replace("      ", "81-162", 36, 6);
-            //        break;
-            //    case 192:
-            //        stringBuild.Replace("       ", "192-384", 36, 7);
-            //        break;
-            //    case 375:
-            //        stringBuild.Replace("       ", "375-750", 36, 7);
-            //        break;
-            //    case 648:
-            //        stringBuild.Replace("        ", "648-1256", 36, 8);
-            //        break;
-            //    case 1029:
-            //        stringBuild.Replace("         ", "1029-2058", 36, 9);
-            //        break;
-            //    case 1536:
-            //        stringBuild.Replace("         ", "1536-3042", 36, 9);
-            //        break;
-            //    case 2187:
-            //        stringBuild.Replace("         ", "2187-4374", 36, 9);
-            //        break;
-            //    case 3000:
-            //        stringBuild.Replace("         ", "3000-6000", 36, 9);
-            //        break;
-            //    case 3993:
-            //        stringBuild.Replace("         ", "3993-7986", 36, 9);
-            //        break;
-            //    case 5184:
-            //        stringBuild.Replace("          ", "5184-10368", 36, 10);
-            //        break;
-            //}
-
-            //if (levelInt < 10)
-            //{
-            //    stringBuild.Replace("_", " ", 193, 1);
-            //    stringBuild.Replace("_", levelInt.ToString(), 194, 1);
-            //}
-            //else
-            //{
-            //    stringBuild.Replace("__", levelInt.ToString(), 193, 2);
-            //}
-
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    int newNum;
-
-            //    if (levelInt == 3 || levelInt == 6)
-            //    {
-            //        switch (randNum.Next(0, 2))
-            //        {
-            //            case 0:
-            //                randSigns.Add('+');
-            //                break;
-            //            case 1:
-            //                randSigns.Add('-');
-            //                break;
-            //        }
-            //        do
-            //        {
-            //            newNum = randNum.Next(0, 3);
-            //        } while (i > 0 && newNum == randNums[i - 1]);
-            //        randNums.Add(newNum);
-            //    }
-            //    else if (levelInt == 9 || levelInt == 12)
-            //    {
-            //        switch (randNum.Next(0, 2))
-            //        {
-            //            case 0:
-            //                randSigns.Add('+');
-            //                break;
-            //            case 1:
-            //                randSigns.Add('-');
-            //                break;
-            //        }
-            //        randNums.Add(randNum.Next(0, 5));
-            //    }
-            //    else if (levelInt == 15 || levelInt == 18)
-            //    {
-            //        switch (randNum.Next(0, 3))
-            //        {
-            //            case 0:
-            //                randSigns.Add('+');
-            //                break;
-            //            case 1:
-            //                randSigns.Add('-');
-            //                break;
-            //            case 2:
-            //                randSigns.Add('×');
-            //                break;
-            //        }
-            //        randNums.Add(randNum.Next(0, 7));
-            //    }
-            //    else if (levelInt == 21 || levelInt == 24)
-            //    {
-            //        switch (randNum.Next(0, 3))
-            //        {
-            //            case 0:
-            //                randSigns.Add('+');
-            //                break;
-            //            case 1:
-            //                randSigns.Add('-');
-            //                break;
-            //            case 2:
-            //                randSigns.Add('×');
-            //                break;
-            //        }
-            //        randNums.Add(randNum.Next(0, 9));
-            //    }
-            //    else if (levelInt == 27 || levelInt == 30)
-            //    {
-            //        switch (randNum.Next(0, 4))
-            //        {
-            //            case 0:
-            //                randSigns.Add('+');
-            //                break;
-            //            case 1:
-            //                randSigns.Add('-');
-            //                break;
-            //            case 2:
-            //                randSigns.Add('×');
-            //                break;
-            //            case 3:
-            //                randSigns.Add('÷');
-            //                break;
-            //        }
-            //        randNums.Add(randNum.Next(0, 11));
-            //    }
-            //    else if (levelInt == 33 || levelInt == 36)
-            //    {
-            //        switch (randNum.Next(0, 4))
-            //        {
-            //            case 0:
-            //                randSigns.Add('+');
-            //                break;
-            //            case 1:
-            //                randSigns.Add('-');
-            //                break;
-            //            case 2:
-            //                randSigns.Add('×');
-            //                break;
-            //            case 3:
-            //                randSigns.Add('÷');
-            //                break;
-            //        }
-            //        randNums.Add(randNum.Next(0, 13));
-            //    }
-            //}
-            //stringBuild.Replace(" ", randSigns[0].ToString(), 82, 1);
-            //stringBuild.Replace(" ", randSigns[1].ToString(), 87, 1);
-            //stringBuild.Replace(" ", randSigns[2].ToString(), 92, 1);
-            //stringBuild.Replace(" ", randNums[0].ToString(), 83, 1);
-            //stringBuild.Replace(" ", randNums[1].ToString(), 88, 1);
-            //stringBuild.Replace(" ", randNums[2].ToString(), 93, 1);
-
-            stringBuild.Replace("__", levelInt.ToString(), 193, 2);
-
-            for (int i = 0; i < 10; i++)
-            {
-                randNums.Add(randNum.Next(0, 10).ToString());
-                stringBuild.Replace("_", randNums[i], 181 + i + i, 1);
-            }
-
-            return stringBuild;
-        }
-        private StringBuilder MathSigns(StringBuilder stringBuild)
-        {
             string[] allSigns = { "+", "-", "×", "÷" };
-            Random rand = new Random();
-            var selectedSigns = allSigns.OrderBy(x => rand.Next()).Take(3).ToList();
+            string[] allNumbers = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            var selectedSigns = allSigns.OrderBy(x => randNum.Next()).Take(3).ToList();
+            var selectedNumbers = allNumbers.OrderBy(x => randNum.Next()).Take(3).ToList();
 
             if (stringBuild.Length > 93)
             {
-                stringBuild.Remove(83, 1).Insert(83, selectedSigns[0]);
-                stringBuild.Remove(88, 1).Insert(88, selectedSigns[1]);
-                stringBuild.Remove(93, 1).Insert(93, selectedSigns[2]);
+                stringBuild.Remove(82, 1).Insert(82, selectedSigns[0]);
+                stringBuild.Remove(87, 1).Insert(87, selectedSigns[1]);
+                stringBuild.Remove(92, 1).Insert(92, selectedSigns[2]);
+                stringBuild.Remove(83, 1).Insert(83, selectedNumbers[0]);
+                stringBuild.Remove(88, 1).Insert(88, selectedNumbers[1]);
+                stringBuild.Remove(93, 1).Insert(93, selectedNumbers[2]);
             }
+
+            int levelInt = ((int)Math.Abs(gameLevel) - 1) * 6 + 12;
+            stringBuild.Remove(191, 2).Insert(190, levelInt.ToString());
 
             return stringBuild;
         }
@@ -504,67 +333,18 @@ Noooooooooooooooooo!""";
             switch (level)
             {
                 case 1.1:
-                case 1.2:
-                case 1.3:
-                case 1.4:
-                case 1.5:
                 case 2.1:
-                case 2.2:
-                case 2.3:
-                case 2.4:
-                case 2.5:
                 case 3.1:
-                case 3.2:
-                case 3.3:
-                case 3.4:
-                case 3.5:
                 case 4.1:
-                case 4.2:
-                case 4.3:
-                case 4.4:
-                case 4.5:
                 case 5.1:
-                case 5.2:
-                case 5.3:
-                case 5.4:
-                case 5.5:
                 case 6.1:
-                case 6.2:
-                case 6.3:
-                case 6.4:
-                case 6.5:
                 case 7.1:
-                case 7.2:
-                case 7.3:
-                case 7.4:
-                case 7.5:
                 case 8.1:
-                case 8.2:
-                case 8.3:
-                case 8.4:
-                case 8.5:
                 case 9.1:
-                case 9.2:
-                case 9.3:
-                case 9.4:
-                case 9.5:
                 case 10.1:
-                case 10.2:
-                case 10.3:
-                case 10.4:
-                case 10.5:
                 case 11.1:
-                case 11.2:
-                case 11.3:
-                case 11.4:
-                case 11.5:
                 case 12.1:
-                case 12.2:
-                case 12.3:
-                case 12.4:
-                case 12.5:
-                    RoomNumbers(roomStringBuilder);
-                    MathSigns(roomStringBuilder);
+                    Randomizer(roomStringBuilder);
                     break;
             }
 
